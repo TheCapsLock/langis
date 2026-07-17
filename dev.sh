4c85635f3dd8574c793e97fcb0023222c9d0e171
CapsLock <git-langis@legeox.net>
2026-07-17 23:30:23 +0200

    fixes for v8.19.2 (thanks to @emilyinspace for the contribution)

--- dev.sh
+++ dev.sh
@@ -8,4 +8,6 @@ echo "Cloning Signal-Android (latest tag=$LATEST_TAG)"
 
 git clone --depth 1 --recurse-submodules -b "$LATEST_TAG" https://github.com/WhisperSystems/Signal-Android
 cd Signal-Android
+#FIXME: not the same as in .gitlab-ci.yml; this one seems more robust
+for f in app/src/main/res/values*/strings.xml; do sed -i 's/\(<string name="app_name"[^>]*>\)[^<]*/\1Langis/g' "$f"; done
 for f in ../0000-*.patch; do echo "$f"; git apply "$f"; done
